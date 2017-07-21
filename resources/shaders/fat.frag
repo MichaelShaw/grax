e63328e29ecf081e2b8d943f58b7d165f07c13b0
Michael Shaw <michaeljshaw@gmail.com>
2017-07-21 19:10:59 +1000

    Immutable texture array works

--- resources/shaders/fat.frag
+++ resources/shaders/fat.frag
@@ -7,7 +7,7 @@ uniform Locals {
 	float u_alpha_minimum;
 };
 
-// uniform sampler2DArray u_texture;
+uniform sampler2DArray u_texture;
 
 in vec4 v_color;
 in vec3 v_tex_coord;
@@ -16,14 +16,14 @@ in vec3 v_normal;
 out vec4 f_color;
 
 void main() {
-    vec4 albedo_colour = v_color;
+    vec4 albedo_colour = texture(u_texture, v_tex_coord) * 0.8 + v_color * 0.2;
 
     vec4 final_colour = albedo_colour; // * light;
-    final_colour.a = albedo_colour.a; // ignore light's alpha
+    final_colour.a = v_color.a; // ignore light's alpha
 
-    if(final_colour.a < 0.01) { // u_alpha_minimum
-        discard;
-    }
+    //if(final_colour.a < 0.001) { // u_alpha_minimum
+     //  discard;
+    //}
     f_color = final_colour;
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,11 @@ extern crate glutin;
 use cgmath::SquareMatrix;
 use gfx::traits::FactoryExt;
 use gfx::Device;
+use gfx::texture;
 use glutin::GlContext;
+use gfx::format::{Depth, Rgba8};
+use image::{GenericImage, RgbaImage};
+use gfx::Factory;
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
@@ -30,7 +34,7 @@ gfx_defines! {
 
     pipeline pipe_no_blend {
         vbuf: gfx::VertexBuffer<Vertex> = (),
-//        cubemap: gfx::TextureSampler<[f32; 4]> = "t_Cubemap",
+        texture: gfx::TextureSampler<[f32; 4]> = "u_texture",
         locals: gfx::ConstantBuffer<Locals> = "Locals",
         out_color: gfx::RenderTarget<ColorFormat> = "Target0",
         out_depth: gfx::DepthTarget<DepthFormat> = gfx::preset::depth::LESS_EQUAL_WRITE,
@@ -38,25 +42,27 @@ gfx_defines! {
 
     pipeline pipe_blend {
         vbuf: gfx::VertexBuffer<Vertex> = (),
+        texture: gfx::TextureSampler<[f32; 4]> = "u_texture",
         locals: gfx::ConstantBuffer<Locals> = "Locals",
         out_color: gfx::BlendTarget<ColorFormat> = ("Target0", gfx::state::ColorMask::all(), gfx::preset::blend::ALPHA),
         out_depth: gfx::DepthTarget<DepthFormat> = gfx::preset::depth::LESS_EQUAL_WRITE,
     }
 }
 
-const TEX_COORD : [f32; 3] = [0.0, 0.0, 0.0];
+const TEX_COORD_A : [f32; 3] = [0.0, 0.0, 0.0];
+const TEX_COORD_B : [f32; 3] = [0.0, 0.0, 1.0];
 const NORMAL : [f32; 3] = [1.0, 0.0, 0.0];
 
 const TRIANGLE: [Vertex; 3] = [
-    Vertex { position: [  -0.5, -0.5, 0.0 ], tex_coord: TEX_COORD, color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
-    Vertex { position: [  0.5, -0.5, 0.0 ], tex_coord: TEX_COORD,  color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
-    Vertex { position: [  0.0,  0.5 , 0.0 ], tex_coord: TEX_COORD, color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
+    Vertex { position: [  -0.5, -0.5, 0.0 ], tex_coord: [  0.0, 0.0, 0.0 ], color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
+    Vertex { position: [  0.5, -0.5, 0.0 ], tex_coord: [  1.0, 0.0, 0.0 ],  color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
+    Vertex { position: [  0.0,  0.5 , 0.0 ], tex_coord: [  0.5, 1.0 , 0.0 ], color: [1.0, 1.0, 1.0, 1.0], normal: NORMAL },
 ];
 
 const TRIANGLE_B: [Vertex; 3] = [
-    Vertex { position: [  -0.3, -0.5, -0.1 ], tex_coord: TEX_COORD, color: [0.0, 0.0, 1.0, 0.5], normal: NORMAL },
-    Vertex { position: [  0.7, -0.5, -0.1 ], tex_coord: TEX_COORD,  color: [0.0, 1.0, 0.0, 0.5], normal: NORMAL },
-    Vertex { position: [  0.2,  0.5 , -0.1 ], tex_coord: TEX_COORD, color: [1.0, 0.0, 0.0, 0.5], normal: NORMAL },
+    Vertex { position: [  -0.3, -0.5, -0.1 ], tex_coord: [  0.0, 0.0, 1.0 ], color: [0.0, 0.0, 1.0, 1.0], normal: NORMAL },
+    Vertex { position: [  0.7, -0.5, -0.1 ], tex_coord: [  1.0, 0.0, 1.0 ],  color: [0.0, 1.0, 0.0, 1.0], normal: NORMAL },
+    Vertex { position: [  0.2,  0.5 , -0.1 ], tex_coord: [  0.5, 1.0 , 1.0 ], color: [1.0, 0.0, 0.0, 1.0], normal: NORMAL },
 ];
 
 pub fn ui_projection(width: f32, height: f32) -> cgmath::Matrix4<f32> {
@@ -98,6 +104,16 @@ pub fn run_example() {
 
     println!("pso done");
 
+    println!("time for textures");
+    let a = image::open("resources/textures/0_layer.png").unwrap().to_rgba().into_raw();
+    let b = image::open("resources/textures/1_layer.png").unwrap().to_rgba().into_raw();
+    let data : Vec<&[u8]> = vec![&a, &b];
+    let kind = texture::Kind::D2Array(64, 64, 2, gfx::texture::AaMode::Single);
+    let (texture, texture_view) = factory.create_texture_immutable_u8::<Rgba8>(kind, &data).unwrap();
+
+    println!("textures done");
+
+
     // we need window dimensions
 //    let transform = ui_projection(width as f32, height as f32);
     let transform = cgmath::Matrix4::identity();
@@ -105,8 +121,11 @@ pub fn run_example() {
     let (vb_a, slice_a) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
     let (vb_b, slice_b) = factory.create_vertex_buffer_with_slice(&TRIANGLE_B, ());
 
+    let sampler = factory.create_sampler_linear();
+
     let mut opaque_data = pipe_no_blend::Data {
         vbuf: vb_a.clone(),
+        texture: (texture_view.clone(), sampler.clone()),
         locals: factory.create_constant_buffer(1),
         out_color: main_color.clone(),
         out_depth: main_depth.clone(),
@@ -114,6 +133,7 @@ pub fn run_example() {
 
     let mut blend_data = pipe_blend::Data {
         vbuf: vb_b.clone(),
+        texture: (texture_view.clone(), sampler.clone()),
         locals: factory.create_constant_buffer(1),
         out_color: main_color.clone(),
         out_depth: main_depth.clone(),
@@ -136,13 +156,20 @@ pub fn run_example() {
                         running = false
                     },
                     glutin::WindowEvent::Resized(width, height) => {
+                        let scale = window.hidpi_factor();
+
+                        println!("resize to {:?} {:?} scale {:?}", width, height, scale);
                         window.resize(width, height);
+
+
                         gfx_window_glutin::update_views(&window, &mut main_color, &mut main_depth);
                     },
                     _ => (),
                 }
             }
         });
+
+
 
         let locals = Locals {
             u_transform: transform.into(),
@@ -160,6 +187,8 @@ pub fn run_example() {
         encoder.draw(&slice_a, &pso, &opaque_data);
         blend_data.vbuf = vb_b.clone();
         encoder.draw(&slice_b, &pso_blend, &blend_data);
+//        blend_data.vbuf = vb_b.clone();
+//        encoder.draw(&slice_b, &pso_blend, &blend_data);
 
 
 
